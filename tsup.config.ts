cf7ede402238301def7196a6fde7c3060857e2e1
Luca Zani <lucazani2002@gmail.com>
2023-10-15 14:06:42 +0200

    feat: only generate esm

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,8 +2,15 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["build/**/*.ts", "build/**/*.tsx"],
-  format: ["cjs", "esm"],
+  format: ["esm"],
   splitting: true,
   dts: true,
   minify: true,
+  treeshake: true,
+  outExtension({ format }) {
+    // Remove the .d.mts extension because it's not working
+    return {
+      dts: `.${format}.d.ts`,
+    };
+  },
 });
